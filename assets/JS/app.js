ab47c902dda1364dbee88858c139848932c08c04
Kirill <kir.suprun@gmail.com>
2023-05-16 18:59:07 +0300

    start

---
+++ assets/JS/app.js
@@ -0,0 +1,42 @@
+console.log(`its work`);
+
+const roads = [
+    "Дом Алисы-Дом Боба",
+    "Дом Алисы-Почта",
+    "Дом Дарии-Дом Энри",
+    "Дом Эрни-Дом Греты",
+    "Дом Греты-Магазин",
+    "Рынок-Почта",
+    "Рынок-Ратуша",
+    "Дом Алисы-Склад",
+    "Дом Боба-Ратуша",
+    "Дом Дарии-Ратуша",
+    "Дом Греты-Ферма",
+    "Рынок-Ферма",
+    "Рынок-Магазин",
+    "Магазин-Ратуша"
+];
+
+console.log(`roads`, roads);
+
+function buildGraph(edges){
+    let graph = Object.create(null);
+    
+    function addEdge(from, to){
+        if(graph[from] == null){
+            graph[from] = [to];
+        } else {
+            graph[from].push[to];
+        }
+    };
+
+    for (let [from, to] of edges.map(r => r.split('-'))){
+        addEdge(from, to);
+        addEdge(to, from);
+    };
+
+    return graph
+}
+
+console.log(`check`, Object.create(null))
+
