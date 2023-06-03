f29e4d5ea4f81acbd977a44100bb31e216b4617e
Kirill <kir.suprun.job@gmail.com>
2023-06-03 20:29:41 +0300

    Update app.js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -77,7 +77,7 @@ function runRobot(state, robot, memory){
         console.log(`Переход в направление ${action.direction}`);
     }
 
-}
+};
 
 const mailRoute = [
     "Дом Алисы", "Сарай", "Дом Алисы", "Дом Боба",
@@ -95,4 +95,30 @@ function routeRobot(state, memory) {
         direction: memory[0],
         memory: memory.slice(1)
     }
-}
+};
+
+function findRoute(graph, from, to){
+    let work = [{at: from, route: []}];
+    for(let i = 0; i < work.length; i++){
+        let {at, route} = work[i];
+        for(let place of graph[at]){
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            };
+        };
+    };
+};
+
+function goalOrientedRobot ({place, parcels}, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if(parcel.place = !place){
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address)
+        };
+    };
+
+    return {direction: route[0], memory: route.slice(1)}
+};
